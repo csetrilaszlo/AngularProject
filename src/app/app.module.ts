8d89a34538a64dff70235810834f3a9da1afb54e
Csetri Laszlo <csetrilaszlo@gmail.com>
2021-08-10 21:18:03 +0300

    creating header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonbarComponent } from './components/buttonbar/buttonbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonbarComponent,
+    
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import { Task } from './Tasks';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'May 6th at 6:30pm',
+        reminder: true,
+    },
+    {
+        id: 1,
+        text: 'Food Shopping',
+        day: 'May 7th at 9:30pm',
+        reminder: false,
+    },
+    
+]
